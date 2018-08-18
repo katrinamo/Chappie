65f32775fe23052140ae4cfcb5b0add04c0088a3
philzeey <trevorcoreyphillips@gmail.com>
2018-08-18 11:27:28 -0700

    Initial commit

---
+++ cogs/utils/dataIO.py
@@ -0,0 +1,37 @@
+import json
+
+
+class DataIO:
+
+    def save_json(self, filename, data):
+        """Atomically saves json file"""
+        self._save_json(filename, data)
+        return True
+
+    def load_json(self, filename):
+        """Loads json file"""
+        return self._read_json(filename)
+
+    def is_valid_json(self, filename):
+        """Verifies if json file exists/ is readable"""
+        try:
+            self._read_json(filename)
+            return True
+        except FileNotFoundError:
+            return False
+        except json.decoder.JSONDecodeError:
+            return False
+
+    def _read_json(self, filename):
+        with open(filename, encoding='utf-8', mode="r") as f:
+            data = json.load(f)
+        return data
+
+    def _save_json(self, filename, data):
+        with open(filename, encoding='utf-8', mode="w") as f:
+            json.dump(data, f, indent=4, sort_keys=True,
+                      separators=(',', ' : '))
+        return data
+
+
+dataIO = DataIO()
