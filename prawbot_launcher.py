92778999ca2f6ce33cc14bb16bdd094cf7cfe9d2
philzeey <trevorcoreyphillips@gmail.com>
2018-08-24 23:57:22 -0600

    Initial commit

---
+++ prawbot_launcher.py
@@ -0,0 +1,14 @@
+from reddit.stream import Stream
+
+
+def run_stream():
+    stream = Stream()
+    stream.run()
+
+
+def main():
+    run_stream()
+
+
+if __name__ == '__main__':
+    main()
---
+++ reddit/stream.py
@@ -0,0 +1,79 @@
+import time
+
+from prawcore.exceptions import PrawcoreException
+from prawbot import Praw
+
+
+class Stream:
+    def __init__(self):
+        self.praw = Praw()
+
+    def stream_task(self):
+        subreddit = self.praw.subreddit("healthanxiety")
+
+        for submission in subreddit.stream.submissions():
+            self.insert_subreddit(submission.subreddit)
+            self.insert_author(submission.author)
+            self.insert_submission(submission)
+
+    def insert_subreddit(self, subreddit):
+
+        subreddit = subreddit.display_name
+
+        query = '''
+                INSERT INTO subreddit (subreddit) VALUES ($1);
+                '''
+        params = (subreddit,)
+
+        self.praw.db.execute(query=query, params=params)
+
+    def insert_author(self, author):
+
+        if author is None:
+            return
+
+        author_name = author.name
+        link_karma = author.link_karma
+        comment_karma = author.comment_karma
+        created = author.created_utc
+
+        query = '''
+                INSERT INTO author (author, link_karma, comment_karma, created) VALUES ($1, $2, $3, $4);
+                '''
+        params = (author_name, link_karma, comment_karma, created)
+
+        self.praw.db.execute(query=query, params=params)
+
+    def insert_submission(self, submission):
+
+        if submission.author is None:
+            return
+
+        sub_id = submission.id
+        subreddit = submission.subreddit.display_name
+        author = submission.author.name
+        title = submission.title
+        body = submission.selftext
+        url = submission.url
+        created = submission.created_utc
+
+        query = '''
+        INSERT INTO submission (sub_id, subreddit, author, title, body, url, created, read) VALUES ($1, $2, $3, $4, $5, $6, $7, DEFAULT);
+        '''
+        params = (sub_id, subreddit, author, title, body, url, created)
+
+        self.praw.db.execute(query=query, params=params)
+
+    def run(self):
+        running = True
+        while running:
+            try:
+                self.stream_task()
+            except KeyboardInterrupt:
+                print('Termination received. Goodbye!')
+                self.praw.db.disconnect()
+                running = False
+            except PrawcoreException:
+                print("PrawcoreException occurred. Sleeping for 10 seconds.")
+                time.sleep(10)
+        return 0
