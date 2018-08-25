6220a41f2322475eebc5edceaf166fa23b81a88e
philzeey <trevorcoreyphillips@gmail.com>
2018-08-25 14:49:21 -0600

    took away spam and added check for updated subscriptions

--- reddit/stream.py
+++ reddit/stream.py
@@ -2,40 +2,65 @@ import time
 
 from prawcore.exceptions import PrawcoreException
 
+from cogs.utils.dataIO import dataIO
 from prawbot import Praw
-from reddit.spam import Spam
+
+
+class SubscriptionsUpdated(Exception):
+    pass
 
 
 class Stream:
     def __init__(self):
         self.praw = Praw()
-        self.spam = Spam()
+        self.settings = dataIO.load_json("data/reddit/settings.json")
+
+    def get_list_of_subs(self):
+        master_list = []
+        for channel_id, subscriptions in self.settings.items():
+            for title, sub_list in subscriptions.items():
+                for subreddit in sub_list:
+                    if subreddit not in master_list:
+                        master_list.append(subreddit)
+        return master_list
 
     def stream_task(self):
-        subreddit = self.praw.subreddit("healthanxiety")
+        subscriptions_list = self.get_list_of_subs()
+        subscriptions = '+'.join(subscriptions_list)
+
+        subreddit = self.praw.subreddit(subscriptions)
 
         for submission in subreddit.stream.submissions():
+
+            # Check if subscriptions have updated. If so, restart.
+            self.settings = dataIO.load_json("data/reddit/settings.json")
+            subscriptions_list = self.get_list_of_subs()
+            check_subscriptions = '+'.join(subscriptions_list)
+
+            if check_subscriptions != subscriptions:
+                raise SubscriptionsUpdated
+
             self.insert_subreddit(submission.subreddit)
             self.insert_author(submission.author)
             self.insert_submission(submission)
 
     def insert_subreddit(self, subreddit):
 
-        subreddit = subreddit.display_name
+        subreddit = subreddit.display_name.lower()
 
         query = '''
                 INSERT INTO subreddit (subreddit) VALUES ($1);
                 '''
         params = (subreddit,)
 
-        self.praw.db.execute(query=query, params=params)
+        self.praw.db.praw_execute(query=query, params=params)
 
     def insert_author(self, author):
 
         if author is None:
             return
 
-        author_name = author.name
+        author_name = author.name.lower()
         link_karma = author.link_karma
         comment_karma = author.comment_karma
         created = author.created_utc
@@ -45,7 +70,7 @@ class Stream:
                 '''
         params = (author_name, link_karma, comment_karma, created)
 
-        self.praw.db.execute(query=query, params=params)
+        self.praw.db.praw_execute(query=query, params=params)
 
     def insert_submission(self, submission):
 
@@ -53,8 +78,8 @@ class Stream:
             return
 
         sub_id = submission.id
-        subreddit = submission.subreddit.display_name
-        author = submission.author.name
+        subreddit = submission.subreddit.display_name.lower()
+        author = submission.author.name.lower()
         title = submission.title
         body = submission.selftext
         url = submission.url
@@ -65,7 +90,7 @@ class Stream:
         '''
         params = (sub_id, subreddit, author, title, body, url, created)
 
-        self.praw.db.execute(query=query, params=params)
+        self.praw.db.praw_execute(query=query, params=params)
 
     def run(self):
         running = True
@@ -76,6 +101,9 @@ class Stream:
                 print('Termination received. Goodbye!')
                 self.praw.db.disconnect()
                 running = False
+            except SubscriptionsUpdated:
+                print("Subscriptions have updated, restarting...")
+                time.sleep(10)
             except PrawcoreException:
                 print("PrawcoreException occurred. Sleeping for 10 seconds.")
                 time.sleep(10)
