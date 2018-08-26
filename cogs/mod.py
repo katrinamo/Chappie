75501e1168901675ebd75a0d891a2c839019a897
philzeey <trevorcoreyphillips@gmail.com>
2018-08-26 01:46:08 -0600

    updated

--- cogs/mod.py
+++ cogs/mod.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import discord
 from discord.ext import commands
 
@@ -7,6 +9,43 @@ class Mod:
 
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, user: discord.Member, *, reason: str = 'Unspecified'):
+        """Bans user."""
+
+        author = ctx.author
+        guild = ctx.guild
+
+        if author == user:
+            await ctx.send("I cannot let you do that. Self-harm is bad \N{PENSIVE FACE}")
+            return
+
+        await ctx.send(f"{user.mention} did '**{reason}**' and got the ban hammer!")
+        await user.send(f"Hey, sorry about this but... you have been banned from **{guild.name}** by {author} for "
+                        f"'**{reason}**'.")
+        await guild.ban(user=user, reason=reason)
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def tempban(self, ctx, user: discord.Member, minutes: int, *, reason: str = 'Unspecified'):
+        """Temporarily bans user."""
+
+        author = ctx.author
+        guild = ctx.guild
+
+        if author == user:
+            await ctx.send("I cannot let you do that. Self-harm is bad \N{PENSIVE FACE}")
+            return
+
+        await ctx.send(f"{user.mention} did '**{reason}**' and got the ban hammer for {minutes} minutes!")
+        await user.send(f"Hey, sorry about this but... you have been temporarily banned from **{guild.name}** by "
+                        f"{author} for '**{reason}**' and duration of {minutes} minutes.")
+        await guild.ban(user=user, reason=reason)
+
+        await asyncio.sleep(minutes * 60)
+        await guild.unban(user=user, reason="Temporary ban lifted.")
 
     @commands.command()
     @commands.has_permissions(kick_members=True)
@@ -20,98 +59,13 @@ class Mod:
             await ctx.send("I cannot let you do that. Self-harm is bad \N{PENSIVE FACE}")
             return
 
+        await ctx.send(f"{user.mention} has been kicked for '**{reason}**'!")
         await guild.kick(user=user, reason=reason)
-        await ctx.send("Done. That felt good.")
-
-    @commands.command()
-    @commands.has_permissions(ban_members=True)
-    async def ban(self, ctx, user: discord.Member, days_to_delete: int = 0, *, reason: str = None):
-        """Bans user."""
-
-        author = ctx.author
-        guild = ctx.guild
-
-        if author == user:
-            await ctx.send("I cannot let you do that. Self-harm is bad \N{PENSIVE FACE}")
-            return
-
-        await guild.ban(user=user, delete_message_days=days_to_delete, reason=reason)
-        await ctx.send("Done. That felt good.")
-
-    @commands.group()
-    @commands.has_permissions(manage_roles=True)
-    async def mute(self, ctx):
-        """Mutes user in the channel/server."""
-
-        if ctx.invoked_subcommand is None:
-            await self.bot.send_cmd_help(ctx)
-
-    @mute.command(name="channel")
-    async def mute_channel(self, ctx, user: discord.Member, channel: discord.TextChannel = None):
-        """Mutes user in a channel."""
-
-        guild = ctx.guild
-
-        if channel is None:
-            channel = ctx.channel
-
-        await channel.set_permissions(user, send_messages=False)
-        await ctx.send(f"User has been muted in {channel.name}")
-        await user.send(f"Hello, you have been **muted** in {channel.name} by the mods of {guild.name}. If you have "
-                        f"questions as to the reason why, please contact the mods. Thank you.")
-
-    @mute.command(name="server")
-    async def mute_server(self, ctx, user: discord.Member):
-        """Mutes user in the server."""
-
-        guild = ctx.guild
-
-        for channel in guild.text_channels:
-            await channel.set_permissions(user, send_messages=False)
-        await ctx.send("User has been muted in all channels.")
-        await user.send(f"Hello, you have been **muted** in all channels by the mods of {guild.name}. If you have "
-                        f"questions as to the reason why, please contact the mods. Thank you.")
-
-    @commands.group()
-    @commands.has_permissions(manage_roles=True)
-    async def unmute(self, ctx):
-        """Unmutes user in the channel/server."""
-
-        if ctx.invoked_subcommand is None:
-            await self.bot.send_cmd_help(ctx)
-
-    @unmute.command(name="channel")
-    async def unmute_channel(self, ctx, user: discord.Member, channel: discord.TextChannel = None):
-        """Unmutes user in a channel."""
-
-        guild = ctx.guild
-
-        if channel is None:
-            channel = ctx.channel
-
-        overwrite_perm = discord.PermissionOverwrite(send_messages=None)
-        await channel.set_permissions(user, overwrite=overwrite_perm)
-        await ctx.send("User has been unmuted in this channel.")
-        await user.send(f"Hello again, it looks like you have been **unmuted** in {channel.name} by the mods of "
-                        f"{guild.name}. Feel free to come and chat.")
-
-    @unmute.command(name="server")
-    async def unmute_server(self, ctx, user: discord.Member):
-        """Unmutes user in the server."""
-
-        guild = ctx.guild
-
-        overwrite_perm = discord.PermissionOverwrite(send_messages=None)
-        for channel in guild.text_channels:
-            await channel.set_permissions(user, overwrite=overwrite_perm)
-        await ctx.send("User has been unmuted in all applicable channels.")
-        await user.send(f"Hello again, it looks like you have been **unmuted** in all channels by the mods of "
-                        f"{guild.name}. Feel free to come and chat.")
 
     @commands.command()
     @commands.has_permissions(manage_messages=True, read_message_history=True)
-    async def purge(self, ctx, amount: int = 100, channel: discord.TextChannel = None):
-        """Purge X amount of messages from channel. Defaults to 100"""
+    async def clear(self, ctx, amount: int, channel: discord.TextChannel = None):
+        """Clear X amount of messages from channel."""
 
         if channel is None:
             channel = ctx.channel
@@ -119,9 +73,60 @@ class Mod:
         await ctx.message.delete()
         await channel.purge(limit=amount)
 
+        msg = await ctx.send(f"I have cleared `{amount} messages` for ya!")
+        await asyncio.sleep(5)
+        await msg.delete()
+
+    @commands.command()
+    @commands.has_permissions(manage_roles=True)
+    async def mute(self, ctx, user: discord.Member):
+        """Mutes user."""
+
+        guild = ctx.guild
+        mute_role = discord.utils.get(guild.roles, name='Muted')
+
+        if mute_role is None:
+            mute_role = await guild.create_role(name='Muted', color=discord.Color.dark_grey())
+
+        await user.add_roles(mute_role)
+        await ctx.send(f"{user.mention} is now muted!")
+
+    @commands.command()
+    @commands.has_permissions(manage_roles=True)
+    async def tempmute(self, ctx, user: discord.Member, minutes: int):
+        """Termporarily mutes user."""
+
+        guild = ctx.guild
+        mute_role = discord.utils.get(guild.roles, name='Muted')
+
+        if mute_role is None:
+            mute_role = await guild.create_role(name='Muted', color=discord.Color.dark_grey())
+
+        await user.add_roles(mute_role)
+        await ctx.send(f"{user.mention} is now muted for {minutes} minutes!")
+
+        await asyncio.sleep(minutes * 60)
+        await ctx.send(f"{user.mention} is now unmuted.")
+        await user.remove_roles(mute_role)
+
+    @commands.command()
+    @commands.has_permissions(manage_roles=True)
+    async def unmute(self, ctx, user: discord.Member):
+        """Unmutes user."""
+
+        guild = ctx.guild
+        mute_role = discord.utils.get(guild.roles, name='Muted')
+
+        if mute_role is None:
+            mute_role = await guild.create_role(name='Muted', color=discord.Color.dark_grey())
+
+        await user.remove_roles(mute_role)
+        await ctx.send(f"{user.mention} is now unmuted.")
+
     @commands.command()
     @commands.has_permissions(manage_nicknames=True)
     async def nickname(self, ctx, user: discord.Member, *, nickname):
+        """Changes nickname of user."""
 
         await user.edit(nick=nickname)
 
