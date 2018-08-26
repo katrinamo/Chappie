746d6407e38b80397aab0a4c41f6a739ec49311c
philzeey <trevorcoreyphillips@gmail.com>
2018-08-26 12:01:10 -0600

    combined

--- cogs/images.py
+++ cogs/images.py
@@ -11,6 +11,7 @@ class Imgur:
         self.bot = bot
         self.CLIENT_ID = self.bot.config["IMGUR_CLIENT_ID"]
         self.CLIENT_SECRET = self.bot.config["IMGUR_CLIENT_SECRET"]
+        self.GIPHY_API_KEY = self.bot.config["GIPHY_API_KEY"]
 
     @commands.group()
     async def imgur(self, ctx):
@@ -61,6 +62,58 @@ class Imgur:
             if result["data"]:
                 random = randint(0, len(result["data"]))
                 await ctx.send(result["data"][random]["link"])
+
+            else:
+                await ctx.send("Your search terms gave no results.")
+
+        except BaseException:
+            await ctx.send("Error.")
+
+    @commands.group()
+    async def gif(self, ctx):
+        """Gif commands."""
+
+        if ctx.invoked_subcommand is None:
+            await self.bot.send_cmd_help(ctx)
+
+    @gif.command()
+    async def top(self, ctx, *, search_terms: str):
+        """Retrieves the first gif from giphy."""
+
+        search_terms = search_terms.replace(" ", "+")
+        url = f"http://api.giphy.com/v1/gifs/search?&api_key={self.GIPHY_API_KEY}&q={search_terms}"
+
+        try:
+            session = self.bot.session
+            async with session.get(url) as r:
+                t = await r.text()
+                result = json.loads(t)
+
+            if result["data"]:
+                await ctx.send(result["data"][0]["url"])
+
+            else:
+                await ctx.send("Your search terms gave no results.")
+
+        except BaseException:
+            await ctx.send("Error.")
+
+    @gif.command()
+    async def random(self, ctx, search_terms: str):
+        """Retrieves a random gif from giphy."""
+
+        search_terms = search_terms.replace(" ", "+")
+        url = f"http://api.giphy.com/v1/gifs/search?&api_key={self.GIPHY_API_KEY}&q={search_terms}"
+
+        try:
+            session = self.bot.session
+            async with session.get(url) as r:
+                t = await r.text()
+                result = json.loads(t)
+
+            if result["data"]:
+                random = randint(0, len(result["data"]))
+                await ctx.send(result["data"][random]["url"])
 
             else:
                 await ctx.send("Your search terms gave no results.")
